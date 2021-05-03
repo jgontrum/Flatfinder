fbc919c9a9599b03def18df90554eeaeb46f1249
Luca Springer <uhyzv@student.kit.edu>
2021-05-03 20:08:54 +0200

    Cleanup WebsiteParser.py

--- WebsiteParser.py
+++ WebsiteParser.py
@@ -5,7 +5,12 @@ import urllib
 import time
 import sys
 import re
-from bs4 import BeautifulSoup
+from beautifulsoup4 import BeautifulSoup
+
+
+def get_time_stamp():
+    return time.strftime("%b %d %Y %H:%M:%S", time.localtime())
+
 
 def parse(site, url):
     try:
@@ -24,80 +29,79 @@ def parse(site, url):
         elif site == 'Immonet':
             return __Immonet(url)
         else:
-            print "Called with unknown website (" + str(site) + "). Leaving now."
+            print("Called with unknown website (" + str(site) + "). Leaving now.")
             sys.exit(1)
-    except Exception, e:
-        print "Failed to catch data for " + str(site) + " with URL " + str(url)
+    except Exception as e:
+        print("Failed to catch data for " + str(site) + " with URL " + str(url))
+
 
 def __eBay(url):
     page = urllib.urlopen(url)
     ebay_page = BeautifulSoup(page.read())
-    ## Most recent offer
+    # Most recent offer
     most_recent_ad = ebay_page.find("li", attrs={"class": "ad-listitem   "})
-    ## Title
+    # Title
     title = most_recent_ad.find("a", attrs={"class": "ad-title"})
-    ## URL
+    # URL
     link = "http://kleinanzeigen.ebay.de" + title.get('href')
-    ## Text
+    # Text
     text = title.get_text()
-    ## Rent
+    # Rent
     rent = most_recent_ad.find("strong").get_text()
-    ## Location
+    # Location
     location = most_recent_ad.find("h3").get_text(" ")
-    ### Process data
-    return {"title": text, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
+    # Process data
+    return {"title": text, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
+
 
 def __WG1Zimmer(url):
     page = urllib.urlopen(url)
     wg_page = BeautifulSoup(page.read())
-    ## Most recent offer
+    # Most recent offer
     most_recent_ad = wg_page.find("div", attrs={"class": "list-details-ad-wrapper CLR "})
-    ## URL
+    # URL
     link = "http://www.wg-gesucht.de/" + most_recent_ad.find("h2").find("a").get("href")
     title = most_recent_ad.find("h2").find("a").get_text().strip()
-    ## Rent
+    # Rent
     rent_raw = most_recent_ad.find("strong", attrs={"class": "list-details-ad-price"})
     rentList = rent_raw.find("a").get_text().split()
-    rent = ""
     if len(rentList) == 3:
         rent = rentList[2]
     else:
         rent = " ".join(rentList)
-    ## Location
+    # Location
     locationList = most_recent_ad.find("p").get_text().strip().split("\n")
-    location = ""
-    if len(locationList) == 2:
-        location =locationList[0].strip()
-    else:
-        location = " ".join(locationList)
-    ### Process data
-    return {"title": title, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
-
-def __WGWohnung(url):
-    page = urllib.urlopen(url)
-    wg_page = BeautifulSoup(page.read())
-    ## Most recent offer
-    most_recent_ad = wg_page.find("div", attrs={"class": "list-details-ad-wrapper CLR "})
-    ## URL
-    link = "http://www.wg-gesucht.de/" + most_recent_ad.find("h2").find("a").get("href")
-    title = most_recent_ad.find("h2").find("a").get_text().strip()
-    ## Rent
-    rent_raw = most_recent_ad.find("strong", attrs={"class": "list-details-ad-price"})
-    rentList = rent_raw.find("a").get_text().split()
-    rent = ""
-    if len(rentList) == 3:
-        rent = rentList[2]
-    else:
-        rent = " ".join(rentList)
-    ## Location
-    locationList = most_recent_ad.find("p").get_text().strip().split("\n")
-    location = ""
     if len(locationList) == 2:
         location = locationList[0].strip()
     else:
         location = " ".join(locationList)
-    ### Process data
-    return {"title": title, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
+    # Process data
+    return {"title": title, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
+
+
+def __WGWohnung(url):
+    page = urllib.urlopen(url)
+    wg_page = BeautifulSoup(page.read())
+    # Most recent offer
+    most_recent_ad = wg_page.find("div", attrs={"class": "list-details-ad-wrapper CLR "})
+    # URL
+    link = "http://www.wg-gesucht.de/" + most_recent_ad.find("h2").find("a").get("href")
+    title = most_recent_ad.find("h2").find("a").get_text().strip()
+    # Rent
+    rent_raw = most_recent_ad.find("strong", attrs={"class": "list-details-ad-price"})
+    rent_list = rent_raw.find("a").get_text().split()
+    if len(rent_list) == 3:
+        rent = rent_list[2]
+    else:
+        rent = " ".join(rent_list)
+    # Location
+    location_list = most_recent_ad.find("p").get_text().strip().split("\n")
+    if len(location_list) == 2:
+        location = location_list[0].strip()
+    else:
+        location = " ".join(location_list)
+    # Process data
+    return {"title": title, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
 
 
 def __WohnungsBoerse(url):
@@ -105,8 +109,8 @@ def __WohnungsBoerse(url):
     page = urllib.urlopen(url)
     find_area = re.compile("geocode\(.*?\}\);", re.DOTALL)
 
-    offer_raw =  find_area.findall(str(page.read()))[0]
-    id = re.findall(r", \d+,", offer_raw)[0].replace(",","").lstrip()
+    offer_raw = find_area.findall(str(page.read()))[0]
+    identificator = re.findall(r", \d+,", offer_raw)[0].replace(",","").lstrip()
 
     tokenized = re.findall(r"'.*?'", offer_raw)
     # Position | Data
@@ -121,59 +125,62 @@ def __WohnungsBoerse(url):
     location = tokenized[0].replace("'","")
     rent = tokenized[3].replace("'", "").replace("&euro", u"€")
     title = tokenized[6].replace("'", "")
-    link = "http://www.wohnungsboerse.net/immodetail/" + id
+    link = "http://www.wohnungsboerse.net/immodetail/" + identificator
 
-    return {"title": title, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
+    return {"title": title, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
+
 
 def __Immowelt(url):
     page = urllib.urlopen(url)
     immow_page = BeautifulSoup(page.read())
-    ## Most recent offer
+    # Most recent offer
     most_recent_offer = immow_page.find("div", {"class": "divObject  listitem_new_wrap"})
-    ## Title
+    # Title
     url = most_recent_offer.find("a")
     title = most_recent_offer.find("h3").get_text()
-    ## URL
+    # URL
     link = "http://www.immowelt.de" + url.get("href")
-    ## Rent
+    # Rent
     rent = most_recent_offer.find("div", {"class": "hardfact"}).get_text()
-    ## Location
+    # Location
     location_raw = most_recent_offer.find("div", {"class": "location location_exact"}).get_text(" ")
     location = ' '.join(location_raw.split())
-    ### Process data
-    return {"title": title, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
+    # Process data
+    return {"title": title, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
+
 
 def __ImmoScout24(url):
     page = urllib.urlopen(url)
     immoc_page = BeautifulSoup(page.read())
-    ## Most recent offer
+    # Most recent offer
     most_recent_offer = immoc_page.find("div", {"class": "resultlist_entry_data"})
-    ## Title
+    # Title
     url = most_recent_offer.find("a")
     title = url.get("title")
-    ## URL
+    # URL
     link = "http://www.immobilienscout24.de" + url.get("href")
-    ## Rent
+    # Rent
     rent = most_recent_offer.find("dd", {"class": "value"}).get_text().split()[0]
-    ## Location
+    # Location
     location = most_recent_offer.find("span", {"class": "street"}).get_text(" ")
-    ### Process data
-    return {"title": title, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
+    # Process data
+    return {"title": title, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
+
 
 def __Immonet(url):
     page = urllib.urlopen(url)
     immonet_page = BeautifulSoup(page.read())
-    ## Most recent offer
+    # Most recent offer
     most_recent_offer = immonet_page.find("div", {"class": "selListItem"})
-    ## Title
+    # Title
     url = most_recent_offer.find("a")
     title = url.get("title")
-    ## URL
+    # URL
     link = "http://www.immonet.de" + url.get("href")
-    ## Rent
+    # Rent
     rent = most_recent_offer.find("span", {"class": "fsLarge"}).get_text()
-    ## Location
+    # Location
     location = most_recent_offer.find("p", {"class": "fsSmall"}).get_text("")
     location = " ".join(location.split())
-    ### Process data
-    return {"title": title, "url": link, "rent": rent, "location": location, "time": time.strftime("%H:%M:%S")}
+    # Process data
+    return {"title": title, "url": link, "rent": rent, "location": location, "time": get_time_stamp()}
